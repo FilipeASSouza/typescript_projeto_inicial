113f3713dd6984bdd29eca31c1dbf2f21813fbc6
Filipe NoteCasa <filipe.sistemas@gmail.com>
2023-05-17 11:49:33 -0300

    feat: integracao com formulario

--- typescript-curso-1-arquivos-iniciais/app/app.ts
+++ typescript-curso-1-arquivos-iniciais/app/app.ts
@@ -1,6 +1,8 @@
-import { Negociacao } from './models/negociacao.js';
+import { NegociacaoController } from './controllers/negociacao-controller.js';
 
-const negociacao = new Negociacao(new Date(), 10, 100);
-console.log(negociacao);
-console.log(negociacao.data);
-console.log(negociacao.volume);
+const controller = new NegociacaoController;
+const form = document.querySelector('.form');
+form.addEventListener('submit', event => {
+    event.preventDefault();//no submit pagina faz o refresh, essa linha cancela
+    controller.adiciona();
+});
