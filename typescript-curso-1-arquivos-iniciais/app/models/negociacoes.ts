dd8ae9196f009e97cb12c4e4153bc4cafd57b3f6
Filipe NoteCasa <filipe.sistemas@gmail.com>
2023-05-18 12:28:56 -0300

    feat: bloqueando edicao lista criando nova lista

--- typescript-curso-1-arquivos-iniciais/app/models/negociacoes.ts
+++ typescript-curso-1-arquivos-iniciais/app/models/negociacoes.ts
@@ -8,6 +8,7 @@ export class Negociacoes{
     }
 
     lista():Array<Negociacao>{
-        return this.negociacoes;
+        //return this.negociacoes; retorna o mesmo endereco de memoria e permite que a lista seja alterada
+        return [...this.negociacoes];//utilizando expred operator, pega cada item individual e coloca na nova lista
     }
 }
--- typescript-curso-1-arquivos-iniciais/dist/js/models/negociacoes.js
+++ typescript-curso-1-arquivos-iniciais/dist/js/models/negociacoes.js
@@ -6,6 +6,7 @@ export class Negociacoes {
         this.negociacoes.push(negociacao);
     }
     lista() {
-        return this.negociacoes;
+        //return this.negociacoes; retorna o mesmo endereco de memoria e permite que a lista seja alterada
+        return [...this.negociacoes]; //utilizando expred operator, pega cada item individual e coloca na nova lista
     }
 }
