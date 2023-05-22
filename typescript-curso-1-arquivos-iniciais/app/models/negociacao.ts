643aaa90073e61effd0a59ac408c80e3864cf206
Filipe NoteCasa <filipe.sistemas@gmail.com>
2023-05-22 16:12:43 -0300

    feat: aplicando programacao defensiva

--- typescript-curso-1-arquivos-iniciais/app/models/negociacao.ts
+++ typescript-curso-1-arquivos-iniciais/app/models/negociacao.ts
@@ -2,10 +2,15 @@ export class Negociacao{
 
     //simplificando construtor
     constructor(
-        readonly data: Date
-        , readonly quantidade: number
-        , readonly valor: number
+        private _data: Date,
+        readonly quantidade: number,
+        readonly valor: number
         ){}
+
+    get data():Date{
+        const data = new Date(this._data.getTime());
+        return data;
+    }
 
     get volume():number{
         return this.quantidade * this.valor;
