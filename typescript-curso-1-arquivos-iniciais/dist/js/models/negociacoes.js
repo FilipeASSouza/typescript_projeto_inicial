1c2d1452610a86c094727584275d30afb45fc55d
Filipe NoteCasa <filipe.sistemas@gmail.com>
2023-05-22 15:43:18 -0300

    feat: simplificando negociacao.ts

--- typescript-curso-1-arquivos-iniciais/dist/js/models/negociacoes.js
+++ typescript-curso-1-arquivos-iniciais/dist/js/models/negociacoes.js
@@ -1,12 +1,12 @@
 export class Negociacoes {
-    constructor() {
-        this.negociacoes = [];
-    }
     adiciona(negociacao) {
         this.negociacoes.push(negociacao);
     }
-    lista() {
+    /*lista():Array<Negociacao>{
         //return this.negociacoes; retorna o mesmo endereco de memoria e permite que a lista seja alterada
-        return [...this.negociacoes]; //utilizando expred operator, pega cada item individual e coloca na nova lista
+        return [...this.negociacoes];//utilizando expred operator, pega cada item individual e coloca na nova lista
+    }*/
+    lista() {
+        return this.negociacoes;
     }
 }
