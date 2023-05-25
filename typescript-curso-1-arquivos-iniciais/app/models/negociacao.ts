83b36dcc2d02b419134517397d646a5299dbec2a
Filipe NoteCasa <filipe.sistemas@gmail.com>
2023-05-25 10:14:58 -0300

    feat: implementando metodo statico

--- typescript-curso-1-arquivos-iniciais/app/models/negociacao.ts
+++ typescript-curso-1-arquivos-iniciais/app/models/negociacao.ts
@@ -1,5 +1,13 @@
 export class Negociacao{
 
+    public static criaDe(dataString :string, quantidadeString :string, valorString :string):Negociacao{
+        const exp = /-/g; //expressao regular
+        const date = new Date(dataString.replace(exp, ','));
+        const quantidade = parseInt(quantidadeString);
+        const valor = parseFloat(valorString);
+        return new Negociacao(date, quantidade, valor);
+    }
+
     //simplificando construtor
     constructor(
         private _data: Date,
--- typescript-curso-1-arquivos-iniciais/dist/js/models/negociacao.js
+++ typescript-curso-1-arquivos-iniciais/dist/js/models/negociacao.js
@@ -5,6 +5,13 @@ export class Negociacao {
         this.quantidade = quantidade;
         this.valor = valor;
     }
+    static criaDe(dataString, quantidadeString, valorString) {
+        const exp = /-/g; //expressao regular
+        const date = new Date(dataString.replace(exp, ','));
+        const quantidade = parseInt(quantidadeString);
+        const valor = parseFloat(valorString);
+        return new Negociacao(date, quantidade, valor);
+    }
     get data() {
         const data = new Date(this._data.getTime());
         return data;
