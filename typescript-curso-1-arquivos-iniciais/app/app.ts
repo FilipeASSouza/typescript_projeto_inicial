10fb5add5259a4cae2232587f2070565cba990ca
Filipe NoteCasa <filipe.sistemas@gmail.com>
2023-05-25 13:01:36 -0300

    fix: variaveis possiveis null e verificando variaveis

--- typescript-curso-1-arquivos-iniciais/app/app.ts
+++ typescript-curso-1-arquivos-iniciais/app/app.ts
@@ -2,7 +2,11 @@ import { NegociacaoController } from './controllers/negociacao-controller.js';
 
 const controller = new NegociacaoController;
 const form = document.querySelector('.form');
-form.addEventListener('submit', event => {
-    event.preventDefault();//no submit pagina faz o refresh, essa linha cancela
-    controller.adiciona();
-});
+if(form){
+    form.addEventListener('submit', event => {
+        event.preventDefault();//no submit pagina faz o refresh, essa linha cancela
+        controller.adiciona();
+    });
+} else{
+    throw Error('Não foi possivel executar a aplicação, verifique se o form existe!');
+}
