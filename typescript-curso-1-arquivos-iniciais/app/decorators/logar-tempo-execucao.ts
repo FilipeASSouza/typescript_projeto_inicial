6a821acf95126973dd4abc2048e6ae1864895f11
Filipe NoteCasa <filipe.sistemas@gmail.com>
2023-05-26 11:36:30 -0300

    feat: finalizando decorator

--- typescript-curso-1-arquivos-iniciais/app/decorators/logar-tempo-execucao.ts
+++ typescript-curso-1-arquivos-iniciais/app/decorators/logar-tempo-execucao.ts
@@ -1,9 +1,19 @@
 export function logarTempoDeExecucao(){
     return function(
-        target: any,
-        propertyKey: string,
+        target: any, //isso informa que pode vir qualquer coisa, uma classe ou metodo
+        propertyKey: string,//nome do metodo
         descriptor: PropertyDescriptor
     ){
+        const metodoOriginal = descriptor.value;
+
+        descriptor.value = function(...args:Array<any>){
+            const t1 = performance.now();
+            const retorno = metodoOriginal.apply(this, args);
+            const t2 = performance.now();
+            console.log(`${propertyKey} tempo de execução: ${(t2 - t1)/1000} em segundos.`);
+            retorno
+        }
+
         return descriptor;
     }
 }
