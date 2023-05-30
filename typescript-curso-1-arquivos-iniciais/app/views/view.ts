37c8a1c4e18e28b60b56339883bde2219a9b4339
Filipe NoteCasa <filipe.sistemas@gmail.com>
2023-05-30 09:57:39 -0300

    feat: escapando texto atraves decorator

--- typescript-curso-1-arquivos-iniciais/app/views/view.ts
+++ typescript-curso-1-arquivos-iniciais/app/views/view.ts
@@ -1,10 +1,9 @@
 import { inspect } from "../decorators/inspect.js";
 import { logarTempoDeExecucao } from "../decorators/logar-tempo-execucao.js";
-
 export abstract class View<T> {
     protected elemento:HTMLElement;
-    private escapar = false;
-
+    //private escapar = false;
+/*
     constructor(seletor :string , escapar? :boolean){
         const elemento = document.querySelector(seletor);
         if(elemento){
@@ -16,15 +15,23 @@ export abstract class View<T> {
         if(escapar){
             this.escapar = escapar;
         }
+    }*/
+    constructor(seletor :string){
+        const elemento = document.querySelector(seletor);
+        if(elemento){
+            this.elemento = elemento as HTMLElement;
+        } else{
+            throw Error(`Seletor ${seletor} não existe no DOM, verifique!`);
+        }
     }
 
     @inspect
     @logarTempoDeExecucao(true)
     public update(model :T) :void{
         let template = this.template(model);
-        if(this.escapar){
+        /*if(this.escapar){
             template = template.replace(/<script>[\s\S]*?<\/script>/, '');
-        }
+        }*/
         this.elemento.innerHTML = template;
     }
 
