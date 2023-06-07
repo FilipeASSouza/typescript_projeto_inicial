e066a10d1a6de39d8f29c4446867f41af4f4b44d
Filipe NoteCasa <filipe.sistemas@gmail.com>
2023-06-07 12:26:47 -0300

    feat: implementando interface comparavel com generics

--- typescript-curso-1-arquivos-iniciais/app/models/negociacoes.ts
+++ typescript-curso-1-arquivos-iniciais/app/models/negociacoes.ts
@@ -1,7 +1,8 @@
+import { Comparavel } from "../interfaces/comparavel.js";
 import { Imprimivel } from "../utils/imprimivel.js";
 import { Negociacao } from "./negociacao.js";
 
-export class Negociacoes implements Imprimivel{
+export class Negociacoes implements Imprimivel, Comparavel<Negociacoes>{
     
     private negociacoes:Array<Negociacao> = [];
     //private negociacoes: Negociacao[]; também é um array
@@ -21,5 +22,9 @@ export class Negociacoes implements Imprimivel{
 
     public paraTexto() :string {
         return JSON.stringify(this.negociacoes, null, 2);
+    }
+
+    public ehIgual(negociacoes: Negociacoes): boolean {
+        return JSON.stringify(this.negociacoes) === JSON.stringify(negociacoes.lista()); 
     }
 }
